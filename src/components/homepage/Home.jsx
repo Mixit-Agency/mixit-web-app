99150464232f1dd8a824dca274a14a60a30967fd
falouix <falouix@gmail.com>
2022-10-22 00:53:59 +0200

    message

---
+++ src/components/homepage/Home.jsx
@@ -0,0 +1,18 @@
+
+
+function Home() {
+  return (
+    <div className="App">
+        <h1>home Page </h1>
+        <div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+  );
+}
+
+export default Home;
